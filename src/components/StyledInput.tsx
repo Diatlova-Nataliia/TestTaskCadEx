7f441ec88bb1b4915dcd4f327f770704c9ccddfc
Diatlova-Nataliia <natasha200124@gmail.com>
2025-05-11 16:07:31 +0900

    Added custom focused state to the input

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -12,10 +12,38 @@ export interface StyledInputProps {
 
 export const StyledInput = styled.textarea<StyledInputProps>`
   padding: 11px 0 11px 16px;
-  border: 1px solid #d9d9d9;
+  border: 3px solid transparent;
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  background-image: linear-gradient(45deg, #d9d9d9, #d9d9d9);
+  background-size:
+    200% 100%,
+    200% 100%;
+  background-position:
+    0 0,
+    0 0;
   font-size: 20px;
   border-radius: 6px;
   font-family: inherit;
   max-width: 270px;
   height: ${({ fieldType = "text" }) => INPUT_HEIGHTS[fieldType]};
+
+  &:focus,
+  &:focus-visible,
+  &:focus-within,
+  &:active {
+    outline: none;
+    border-color: transparent;
+    animation: shine 4s linear infinite;
+    background-image: linear-gradient(white, white),
+      linear-gradient(60deg, #ff0066, #ffcc00, #33ccff, #ff0066);
+  }
+
+  @keyframes shine {
+    to {
+      background-position:
+        0 0,
+        200% 0;
+    }
+  }
 `;
